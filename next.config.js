00b378a2925826b107ff02db5be0c58c43c923cc
Jeroen Ooms <jooms@xebia.com>
2021-08-26 10:24:27 +0200

    Demo page

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
 module.exports = {
   reactStrictMode: true,
-}
+  images: {
+    deviceSizes: [320, 420, 720, 1080],
+    imagesSizes: [100, 250, 400, 600, 800],
+    minimumCacheTTL: 6000,
+  },
+};

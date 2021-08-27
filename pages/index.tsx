c2bf25acade6b2af5a8c0a61b4fa6033a273de26
Jeroen Ooms <jooms@xebia.com>
2021-08-27 16:24:10 +0200

    Update hero with images and descriptions

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,105 +1,18 @@
 import Head from "next/head";
-import Image from "next/image";
-import { ReactElement, useEffect, useState } from "react";
-import styles from "../styles/Home.module.scss";
-
-interface ImageCardProps {
-  image: number;
-}
-
-const ImageCard = ({ image }: ImageCardProps): ReactElement => (
-  <div className={styles.card}>
-    <Image
-      src={`/images/${image}.jpg`}
-      width="300"
-      height="300"
-      alt="Image"
-      layout="responsive"
-      loading="eager"
-      sizes="20vw"
-    />
-  </div>
-);
-
-const InfoCard = () => (
-  <div className={styles.cardInfo}>
-    <div className={styles.info}>
-      <div className={styles.infoContent}>
-        <div className={styles.avatarContainer}>
-          <Image
-            src="/images/avatar.jpg"
-            className={styles.avatar}
-            alt="Jeroen Ooms - frontend developer"
-            layout="responsive"
-            width="100"
-            height="100"
-            priority={true}
-            sizes="250px"
-            quality="100"
-          />
-        </div>
-        <h1>Hi, my name is Jeroen Ooms</h1>
-        <h2>
-          I&apos;m a Dutch front-end developer.
-          <br />
-          Besides{" "}
-          <a
-            href="https://github.com/jeroenoomsNL"
-            target="_blank"
-            rel="noreferrer"
-            title="Jeroen Ooms @ Github"
-          >
-            code
-          </a>{" "}
-          I like tech, running, comics and craft beer.
-        </h2>
-        <div className={styles.links}>
-          <a
-            href="https://twitter.com/jeroenooms"
-            target="_blank"
-            rel="noreferrer"
-            title="Jeroen Ooms @ Twitter"
-          >
-            <span className={styles.tooltip}>Follow me on Twitter</span>{" "}
-            <i className="fa fa-twitter"></i>
-          </a>
-          <a
-            href="https://nl.linkedin.com/in/jeroenooms"
-            target="_blank"
-            rel="noreferrer"
-            title="Jeroen Ooms @ Linkedin"
-          >
-            <span className={styles.tooltip}>Visit my Linkedin profile</span>{" "}
-            <i className="fa fa-linkedin"></i>
-          </a>
-          <a
-            href="https://github.com/jeroenoomsNL"
-            target="_blank"
-            rel="noreferrer"
-            title="Jeroen Ooms @ Github"
-          >
-            <span className={styles.tooltip}>Visit my Github profile</span>{" "}
-            <i className="fa fa-github"></i>
-          </a>
-        </div>
-      </div>
-    </div>
-  </div>
-);
+import { useState } from "react";
+import { Hero } from "../components/Hero";
 
 export default function Home() {
-  const images: number[] = Array.from(Array(20).keys());
-  const [imageSet, setImages] = useState([...images].slice(0, 12));
-
-  useEffect(() => {
-    setImages([...images].sort(() => 0.5 - Math.random()).slice(0, 12));
-  }, []);
+  const [gameActive, setGameStatus] = useState(false);
 
   return (
-    <div className={styles.container}>
+    <>
       <Head>
-        <title>Jeroen Ooms - Dutch web dev</title>
-        <meta name="description" content="Jeroen Ooms, web dev" />
+        <title>Jeroen Ooms / Dutch web dev</title>
+        <meta
+          name="description"
+          content="Jeroen Ooms, Dutch web dev who likes code, tech, running and craf beers"
+        />
         <link rel="icon" href="/favicon.ico" />
         <link
           rel="stylesheet"
@@ -107,13 +20,8 @@ export default function Home() {
         />
       </Head>
       <main>
-        <div className={styles.cards}>
-          {imageSet.map((image) => (
-            <ImageCard image={image} key={`image-${image}`} />
-          ))}
-          <InfoCard />
-        </div>
+        <Hero />
       </main>
-    </div>
+    </>
   );
 }

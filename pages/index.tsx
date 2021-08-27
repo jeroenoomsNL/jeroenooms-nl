5970ccf322906a3ee892b761121bc308ee4d2685
Jeroen Ooms <jooms@xebia.com>
2021-08-27 22:30:29 +0200

    Remove fontawesome css link

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,10 +14,6 @@ export default function Home() {
         />
         <link rel="icon" href="/favicon.ico" />
         <link rel="canonical" href="https://www.jeroenooms.nl" />
-        <link
-          rel="stylesheet"
-          href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css"
-        />
       </Head>
       <main>
         <Hero />

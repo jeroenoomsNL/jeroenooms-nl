bb9020d68768a2b9ea462096d3185b78c567100f
Jeroen Ooms <info@jeroenooms.nl>
2021-11-16 22:09:53 +0100

    Send Web Vitals metrics to Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,34 @@
+import { NextWebVitalsMetric } from "next/app";
+import Script from "next/script";
+import * as gtag from "../lib/gtag";
 import "../styles/globals.scss";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+const MyApp = ({ Component, pageProps }) => (
+  <>
+    <Script
+      strategy="afterInteractive"
+      src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+    />
+    <Script
+      id="gtag-init"
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+        __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+      }}
+    />
+    <Component {...pageProps} />
+  </>
+);
+
+export function reportWebVitals(metric: NextWebVitalsMetric) {
+  console.log(metric);
 }
 
 export default MyApp;

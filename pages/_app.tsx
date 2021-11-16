a34a68f7583cd83da85d223cbbff6ec4a79052a6
Jeroen Ooms <info@jeroenooms.nl>
2021-11-16 22:26:35 +0100

    Send Web Vitals metrics to Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -30,13 +30,15 @@ const MyApp = ({ Component, pageProps }) => (
 export function reportWebVitals({ id, name, label, value }) {
   // Use `window.gtag` if you initialized Google Analytics as this example:
   // https://github.com/vercel/next.js/blob/canary/examples/with-google-analytics/pages/_document.js
-  window?.gtag("event", name, {
-    event_category:
-      label === "web-vital" ? "Web Vitals" : "Next.js custom metric",
-    value: Math.round(name === "CLS" ? value * 1000 : value), // values must be integers
-    event_label: id, // id unique to current page load
-    non_interaction: true, // avoids affecting bounce rate.
-  });
+  if (typeof window !== "undefined") {
+    window?.gtag("event", name, {
+      event_category:
+        label === "web-vital" ? "Web Vitals" : "Next.js custom metric",
+      value: Math.round(name === "CLS" ? value * 1000 : value), // values must be integers
+      event_label: id, // id unique to current page load
+      non_interaction: true, // avoids affecting bounce rate.
+    });
+  }
 }
 
 export default MyApp;

322ab858529cf8122b6923fa0cda1e18afe11f8c
Jeroen Ooms <info@jeroenooms.nl>
2026-02-05 18:55:24 +0100

    Update dependencies

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
   reactStrictMode: true,
   images: {
     deviceSizes: [320, 420, 720, 1080],
-    imagesSizes: [100, 250, 400, 600, 800],
+    imageSizes: [100, 250, 400, 600, 800],
     minimumCacheTTL: 365 * 24 * 60 * 60,
   },
 };
